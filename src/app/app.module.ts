abf5042e7eae3a4f9c8a0b7adcb16a4cb3de0497
Ashwin <ashwindeepak1997@gmail.com>
2024-08-06 10:20:36 +0530

    ver1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { AdComponent } from './ad/ad.component';
+import { EditComponent } from './edit/edit.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    EmployeeComponent,
+    AdComponent,
+    EditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
